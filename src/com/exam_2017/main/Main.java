fbf62af06c0c0290a186d06ff1049dbe1b65cfdb
karlinglee93 <459607025@qq.com>
2018-08-15 21:19:18 +0800

    网络走法数目，递归，未做出来

--- src/com/exam_2017/main/Main.java
+++ src/com/exam_2017/main/Main.java
@@ -6,6 +6,31 @@ import org.junit.Test;
 
 public class Main {
 
+	// 网格走法数目（不会）
+	// 链接：https://www.nowcoder.com/questionTerminal/e27b9fc9c0ec4a17a5064fb6f5ab13e4
+	// 这是一个典型的递归算法问题，因为每次走法必须是往右或者是往下，因此不管是到达那个点，它的必经之路一定是它上方或者左方相邻的那个点，
+	// 因此可以得出一个递归表达式：走法数目f[m][n]=f[m-1][n]+f[m][n-1]；
+	// 这个递归表达式的条件为m,n都不为0的时候，除了这个表达式还需要写出一个已知条件，
+	// f[0][0] = 0;f[i][0] = 1;f[0][j] = 1;这样就可以解出最终f[m][n]的值了
+	// 总结，该递归思路还是要慢慢理解的。而且System.out一定要在方法里写
+	// 且main函数是static类型的，它所调用的方法也要static类型
+	@Test
+	public void test2() {
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		System.out.println(countNum(x, y));
+	}
+
+	public int countNum(int x, int y) {
+		if (x == 0 && y == 0) {
+			return 0;
+		} else if (x == 0 || y == 0) {
+			return 1;
+		}
+		return countNum(x - 1, y) + countNum(x, y - 1);
+	}
+
 	// 解救小易
 	@Test
 	public void test() {
